9117a19779cd77408762367633b711d1b2fc3711
Nishi311 <michaelnishida@gmail.com>
2019-02-02 16:40:36 -0500

    Extension of final report with raw data on true/false pos/negs.

--- CrackRecognition/scripts/validation_script.py
+++ CrackRecognition/scripts/validation_script.py
@@ -19,19 +19,19 @@ class ValidationModule(object):
 
     def __init__(self):
         self.baseline_location = ""
-        self.base_true_pos_list = set()
-        self.base_true_neg_list = set()
+        self.base_true_pos_list = []
+        self.base_true_neg_list = []
 
         self.reports_location = ""
-        self.classified_pos_list = set()
-        self.classified_neg_list = set()
+        self.classified_pos_list = []
+        self.classified_neg_list = []
 
         self.image_type = ".jpg"
 
-        self.num_true_pos = 0
-        self.num_true_neg = 0
-        self.num_false_pos = 0
-        self.num_false_neg = 0
+        self.true_pos_list = []
+        self.true_neg_list = []
+        self.false_pos_list = []
+        self.false_neg_list = []
         self.num_total_reports = 0
         self.sub_image_detection_threshold = 0.5
 
@@ -94,9 +94,9 @@ class ValidationModule(object):
 
         # Strip away all paths and extension types and leave bare file name for pos / neg list.
         for photo_name in temp_pos_list:
-            self.base_true_pos_list.add(os.path.split(os.path.splitext(photo_name)[0])[1])
+            self.base_true_pos_list.append(os.path.split(os.path.splitext(photo_name)[0])[1])
         for photo_name in temp_neg_list:
-            self.base_true_neg_list.add(os.path.split(os.path.splitext(photo_name)[0])[1])
+            self.base_true_neg_list.append(os.path.split(os.path.splitext(photo_name)[0])[1])
 
     def gather_classified_list(self):
         # Go one level down from the given directory to find actual reports.
@@ -105,7 +105,7 @@ class ValidationModule(object):
 
         # Generate a list of all final reports (one for each photo)
         for report_path in list_of_report_paths:
-            if self.REPORT_STRING in report_path:
+            if self.CLASS_REPORT_DESIGNATION_STRING in report_path:
                 final_report_path_list.add(report_path)
         # Go through the report and find out how many sub-images were flagged
         # as positive versus negative.
@@ -117,46 +117,55 @@ class ValidationModule(object):
             report_lines = open(report_path).readlines()
             for line in report_lines:
                 if self.CLASS_REPORT_NAME_STRING in line:
-                    report_name = line.split(self.CLASS_REPORT_NAME_STRING)[1]
+                    # Get rid of trailing new line and leading space in report name.
+                    report_name = line.split(self.CLASS_REPORT_NAME_STRING)[1].rstrip().lstrip()
                 if self.CLASS_REPORT_NEG_STRING in line:
                     num_neg_sub_image = int(line.split(self.CLASS_REPORT_NEG_STRING)[1])
                 if self.CLASS_REPORT_POS_STRING in line:
                     num_pos_sub_image = int(line.split(self.CLASS_REPORT_POS_STRING)[1])
 
             if (num_pos_sub_image / (num_pos_sub_image+num_neg_sub_image)) >= self.sub_image_detection_threshold:
-                self.classified_pos_list.add(report_name)
+                self.classified_pos_list.append(report_name)
             else:
-                self.classified_neg_list.add(report_name)
+                self.classified_neg_list.append(report_name)
 
     def calculate_stats(self):
         for report in self.base_true_pos_list:
             if report in self.classified_pos_list:
-                self.num_true_pos += 1
+                self.true_pos_list.append(report)
             if report in self.classified_neg_list:
-                self.num_false_neg += 1
+                self.false_neg_list.append(report)
         
         for report in self.base_true_neg_list:
             if report in self.classified_pos_list:
-                self.num_false_pos += 1
+                self.false_pos_list.append(report)
             if report in self.classified_neg_list:
-                self.num_true_neg += 1 
+                self.true_neg_list.append(report)
         
-        self.num_total_reports = self.num_true_pos + self.num_true_neg + self.num_false_pos + self.num_false_neg
+        self.num_total_reports = len(self.true_pos_list + self.true_neg_list + self.false_pos_list + self.false_neg_list)
         
-        self.accuracy_percentage = float((self.num_true_pos + self.num_true_neg) / self.num_total_reports)
-        self.recall_percentage = float(self.num_true_pos / (self.num_true_pos + self.num_false_neg))
-        self.precision_percentage = float(self.num_true_pos / (self.num_true_pos + self.num_false_pos))
+        self.accuracy_percentage = float(len(self.true_pos_list + self.true_neg_list) / self.num_total_reports)
+        self.recall_percentage = float(len(self.true_pos_list) / len(self.true_pos_list + self.false_neg_list))
+        self.precision_percentage = float(len(self.true_pos_list) / len(self.true_pos_list + self.false_pos_list))
         
     def generate_report(self):
-        if not os.path.exist(self.FINAL_REPORT_LOCATION):
+        if not os.path.exists(self.FINAL_REPORT_LOCATION):
             os.makedirs(self.FINAL_REPORT_LOCATION)
         os.chdir(self.FINAL_REPORT_LOCATION)
-
         final_report = open(self.FINAL_REPORT_NAME, "w+")
-        final_report.write("Report Time: {0}\n".format(datetime.datetime.now()))
-        final_report.write("Accuracy Rating: {0}\n".format(self.accuracy_percentage))
-        final_report.write("Recall Rating: {0}\n".format(self.recall_percentage))
-        final_report.write("Precision Rating {0}\n".format(self.precision_percentage))
+
+        calculation_results_string = "Report Time: {0}\nAccuracy Rating: {1}\nRecall Rating: {2}\n" \
+                                     "Precision Rating {3}\n\n".format(datetime.datetime.now(), self.accuracy_percentage,
+                                                                     self.recall_percentage, self.precision_percentage)
+        final_report.write(calculation_results_string)
+
+        raw_results_string = "Num True Pos: {0}\nTrue Pos List:\n{1}\n\nNum True Neg: {2}\nTrue Neg List: \n{3}\n\n" \
+                             "Num False Pos: {4}\nFalse Pos List:\n{5}\n\nNum False Neg: {6}\nFalse Neg List:\n{7}\n\n"\
+                             .format(len(self.true_pos_list), self.true_pos_list,
+                                     len(self.true_neg_list), self.true_neg_list,
+                                     len(self.false_pos_list), self.false_pos_list,
+                                     len(self.false_neg_list), self.false_neg_list)
+        final_report.write(raw_results_string)
 
         final_report.close()
 
