eecb67183e60666d1138e23007a676e8920fe779
Nishi311 <michaelnishida@gmail.com>
2019-04-02 18:55:52 -0400

    Implementation of config file generation and filling out quadrant object.

--- Interfaces/quadrant_handling/js_quadrant_handler.py
+++ Interfaces/quadrant_handling/js_quadrant_handler.py
@@ -1,7 +1,7 @@
 import os
-import shutil
 
 from .quadrant_object import Quadrant
+
 
 class JSQuadrantHandler(object):
 
@@ -10,31 +10,34 @@ class JSQuadrantHandler(object):
     def __init__(self):
         self.config_file_name = "quadrant_"
         self.config_path = os.path.join(self.THIS_FILE_PATH, "..", "configs", self.config_file_name)
-
+        self.quadrant_list = []
 
     def run_module(self, raw_js_string):
-        self.parse_raw_input(raw_js_string)
+        self.quadrant_list = self.parse_raw_input(raw_js_string)
 
     def parse_raw_input(self, raw_js_string):
         # Data comes in in a big string like this:
-        # "<div class="grid-item" id="Quadrant 4" data-left="12" data-right="6" data-top="12" data-bottom="6">Quadrant 4</div>
-        # <div class="grid-item" id="Quadrant 3" data-left="6" data-right="0" data-top="12" data-bottom="6">Quadrant 3</div>
-        # <div class="grid-item" id="Quadrant 2" data-left="12" data-right="6" data-top="6" data-bottom="0">Quadrant 2</div>
-        # <div class="grid-item examined-next" id="Quadrant 1" data-left="6" data-right="0" data-top="6" data-bottom="0">Quadrant 1</div>"
+        #
+        # <div class="grid-item examined-next" id="Quadrant 6" lat_limit_left="1" long_limit_left="1" lat_limit_right="1.665361236570813" long_limit_right="1.665361236570813" top_limit="2" bottom_limit="1"></div>
+        # <div class="grid-item examined-next" id="Quadrant 5" lat_limit_left="1.665361236570813" long_limit_left="1.665361236570813" lat_limit_right="2.330722473141626" long_limit_right="2.330722473141626" top_limit="2" bottom_limit="1"></div>
+        # <div class="grid-item examined-next" id="Quadrant 4" lat_limit_left="2.330722473141626" long_limit_left="2.330722473141626" lat_limit_right="2.996083709712439" long_limit_right="2.996083709712439" top_limit="2" bottom_limit="1"></div>
+        # <div class="grid-item examined-next" id="Quadrant 3" lat_limit_left="2.996083709712439" long_limit_left="2.996083709712439" lat_limit_right="3.661444946283252" long_limit_right="3.661444946283252" top_limit="2" bottom_limit="1"></div>
+        # <div class="grid-item examined-next" id="Quadrant 2" lat_limit_left="3.661444946283252" long_limit_left="3.661444946283252" lat_limit_right="4.326806182854066" long_limit_right="4.326806182854066" top_limit="2" bottom_limit="1"></div>
+        # <div class="grid-item examined-next" id="Quadrant 1" lat_limit_left="4.326806182854066" long_limit_left="4.326806182854066" lat_limit_right="4.992167419424879" long_limit_right="4.992167419424879" top_limit="2" bottom_limit="1"></div>"
         # And it needs to be cut down to workable pieces.
 
         # Step one, create a list of strings corresponding to each grid. Chuck out the first bit of the list as it'll
         # only have "<div class="grid-item"" or some such.
         # Will look something like this:
-        # "Quadrant 4" data-left="12" data-right="6" data-top="12" data-bottom="6">Quadrant 4</div><div class="grid-item"
+        # "'"Quadrant 6" lat_limit_left="1" long_limit_left="1" lat_limit_right="1.665361236570813" long_limit_right="1.665361236570813" top_limit="2" bottom_limit="1"></div><div class="grid-item examined-next"
         raw_quad_list = raw_js_string.split("id=")
         raw_quad_list.pop(0)
 
         # Cut off any extraneous bits at the end to look like this:
-        # "Quadrant 4" data-left="12" data-right="6" data-top="12" data-bottom="6"
+        # '"Quadrant 4" lat_limit_left="2.330722473141626" long_limit_left="2.330722473141626" lat_limit_right="2.996083709712439" long_limit_right="2.996083709712439" top_limit="2" bottom_limit="1"></div><div class="grid-item examined-next" '
         refined_quad_strings = []
         for raw_quad_string in raw_quad_list:
-            refined_quad_strings.append(raw_quad_string.split(">Quadrant")[0])
+            refined_quad_strings.append(raw_quad_string.split("></div>")[0])
 
         # Create a list of quadrant objects for easy use.
         quadrant_objects = []
@@ -42,5 +45,12 @@ class JSQuadrantHandler(object):
             new_quad_object = Quadrant()
             new_quad_object.parse_js_string(refined_quad_string)
             quadrant_objects.append(new_quad_object)
+
+        return quadrant_objects
+
+    def write_quadrants_to_config(self):
+        with open(self.config_path, "w") as config_file:
+            for quadrant in self.quadrant_list:
+                config_file.print(quadrant.generate_string())
 
 
--- Interfaces/quadrant_handling/quadrant_object.py
+++ Interfaces/quadrant_handling/quadrant_object.py
@@ -2,47 +2,99 @@
 class Quadrant(object):
     def __init__(self):
         self.quadrant_name = ""
-        self.coord_dict = {"left_latitude": 0,
-                           "left_longitude": 0,
+        self.coord_dict = {"left_latitude_DD": 0,
+                           "left_longitude_DD": 0,
 
-                           "right_latitude": 0,
-                           "right_longitude": 0,
+                           "right_latitude_DD": 0,
+                           "right_longitude_DD": 0,
 
-                           "top_altitude": 0,
-                           "bottom_altitude": 0}
+                           "top_altitude_Meters": 0,
+                           "bottom_altitude_Meters": 0}
 
     def parse_js_string(self, js_string):
         """
         Parses the given refined JS string and sets the parameters of the object.
         :param js_string: (string) -> MUST be in the format:
-                           "Quadrant 4" data-left="12" data-right="6" data-top="12" data-bottom="6"
+                           "Quadrant 56" lat_limit_left="1" long_limit_left="1" lat_limit_right="1.07"
+                           long_limit_right="1.07" top_limit="5" bottom_limit="1"
         """
-        name_string, data_left_on = js_string.split("data-left=")
-        data_left, data_right_on = data_left_on.split("data-right=")
-        data_right, data_top_on = data_right_on.split("data-top=")
-        data_top, data_bottom = data_top_on.split("data-bottom=")
+        name_string, lat_left_limit_on = js_string.split("lat_limit_left=")
+        lat_left_limit, long_left_limit_on = lat_left_limit_on.split("long_limit_left=")
+        long_left_limit, right_lat_limit_on = long_left_limit_on.split("lat_limit_right=")
+        right_lat_limit, right_long_limit_on = right_lat_limit_on.split("long_limit_right=")
+        right_long_limit, top_limit_on = right_lat_limit_on.split("top_limit=")
+        top_limit, bottom_limit = right_long_limit_on.split("bottom_limit=")
 
         self.quadrant_name = name_string.replace('\"', '')
-        data_left = data_left.replace('\"', '')
-        data_right = data_right.replace('\"', '')
-        data_top = data_top.replace('\"', '')
-        data_bottom = data_bottom.replace('\"', '')
+        self.coord_dict["left_latitude_DD"] = lat_left_limit.replace('\"', '')
+        self.coord_dict["left_longitude_DD"] = long_left_limit.replace('\"', '')
+        self.coord_dict["right_latitude_DD"] = right_lat_limit.replace('\"', '')
+        self.coord_dict["right_longitude_DD"] = right_long_limit.replace('\"', '')
+        self.coord_dict["top_altitude_Meters"] = top_limit.replace('\"', '')
+        self.coord_dict["bottom_altitude_Meters"] = bottom_limit.replace('\"', '')
 
-        self.coord_dict["left_latitude"] = data_top.replace('\"', '')
-        self.coord_dict["left_longitude"] = 0
+    def parse_config_string(self, config_string):
+        name_string, left_limit_on = config_string.split("Left_Limit (DD): ")
+        left_limit, right_limit_on = left_limit_on.split("Right_Limit (DD):")
+        right_limit, top_limit_on = right_limit_on.split("Top_Limit (m): ")
+        top_limit, bottom_limit= top_limit_on.split("Bottom_Limit: ")
 
-        self.coord_dict["right_latitude"] = data_bottom.replace('\"', '')
-        self.coord_dict["right_longitude"] = 0
+        self.quadrant_name = name_string.split("Quadrant Name: ")[1]
 
-        self.coord_dict["top_altitude"] = 0
-        self.coord_dict["bottom_altitude"] = 0
+        self.coord_dict["left_latitude_DD"] = left_limit.replace('\"', '').split(",")[0]
+        self.coord_dict["left_longitude_DD"] = left_limit.replace('\"', '').split(",")[1]
+        self.coord_dict["right_latitude_DD"] = right_limit.replace('\"', '').split(",")[0]
+        self.coord_dict["right_longitude_DD"] = right_limit.replace('\"', '').split(",")[1]
+        self.coord_dict["top_altitude_Meters"] = top_limit
+        self.coord_dict["bottom_altitude_Meters"] = bottom_limit
 
-
-    def parse_config_string(self,config_string):
-        pass
-
-    def print_string(self):
-        pass
+    def generate_string(self):
+        output_string = "Quadrant Name: {0}\nLeft_Limit (DD): ({1}, {2})\nRight_Limit (DD): ({3}, {4})\nTop_Limit (m): " \
+                        "{5}\nBottom_Limit: {6}\n".formt(self.quadrant_name,
+                                                         self.coord_dict["left_latitude_DD"],
+                                                         self.coord_dict["left_longitude_DD"],
+                                                         self.coord_dict["right_latitude_DD"],
+                                                         self.coord_dict["right_longitude_DD"],
+                                                         self.coord_dict["top_altitude_Meters"],
+                                                         self.coord_dict["bottom_altitude_Meters"])
+        return output_string
 
     def check_coordinates(self, coordinate_list):
-        pass
+        """
+        Checks to see if a given coordinate string is within the bounds of this quadrant.
+        :param coordinate_list: (list) -> MUST be in the format [[latitude (DD)], [longitude (DD)], [altitude (m)]]
+        :return: (bool) -> True: The coordinate string IS within the bounds of this quadrant
+                           False: The coordinate string is NOT within the bounds of this quadrant
+        """
+        if isinstance(coordinate_list, list):
+            if len(coordinate_list) == 3:
+                given_lat_coord_dd = coordinate_list[0]
+                given_long_coord_dd = coordinate_list[1]
+                given_alt_meters = coordinate_list[2]
+
+                if not self.range_check(self.coord_dict["left_latitude_DD"], self.coord_dict["left_longitude_DD"],
+                                        given_lat_coord_dd):
+                    return False
+                if not self.range_check(self.coord_dict["right_latitude_DD"], self.coord_dict["right_longitude_DD"],
+                                        given_long_coord_dd):
+                    return False
+                if not self.range_check(self.coord_dict["top_altitude_Meters"], self.coord_dict["bottom_altitude_Meters"],
+                                        given_alt_meters):
+                    return True
+                return True
+
+    @staticmethod
+    def range_check(left_coord, right_coord, checking_coord):
+        """
+        real quick function to check range. Because left and right sides of the quad might be swapped due to perspective,
+        need to check ranges from both sides for a value in between.
+        :param left_coord: left hand side of the range.
+        :param right_coord: right hand side of the range.
+        :param checking_coord: Value to check against
+        :return: (bool) -> True: checking_coord in within the range of left and right.
+                           False: checking_coord is NOT within the range of left and right.
+        """
+        if (left_coord < checking_coord < right_coord) or (left_coord > checking_coord > right_coord):
+            return True
+        else:
+            return False
